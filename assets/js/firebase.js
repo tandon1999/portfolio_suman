e38793cbe071d7ca771bb54a2dae3faaade83c6f
SUMAN TANDAN <sumantandan1999@gmail.com>
2023-04-16 20:42:25 +0530

    first commit

---
+++ assets/js/firebase.js
@@ -0,0 +1,54 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-2CgVD0bYqF2jUbOJSpJsJ1jq2XG13O8",
+    authDomain: "contactpage-portfolio.firebaseapp.com",
+    databaseURL: "https://contactpage-portfolio-default-rtdb.firebaseio.com",
+    projectId: "contactpage-portfolio",
+    storageBucket: "contactpage-portfolio.appspot.com",
+    messagingSenderId: "265893089117",
+    appId: "1:265893089117:web:84879b0001af9e331622b4",
+    measurementId: "G-77HP82ZW20"
+  };
+
+  //initialize firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //refrence
+  var contactFormDB = firebase.database().ref("contactForm");
+
+document.getElementById('contactForm').addEventListener("submit", submitForm);
+
+function submitForm(e)
+{
+    e.preventDefault();
+    var name=getElementVal('name');
+    var email=getElementVal('emailid');
+    var subject=getElementVal('subject');
+    var message=getElementVal('message');
+
+    saveMessages(name, emailid,subject,message);
+    
+    document.querySelector(".alert").style.display = "block";
+
+    setTimeout(() => {
+        document.querySelector(".alert").style.display = "none";
+      }, 1000);
+    
+      //   reset the form
+      document.getElementById("contactForm").reset();
+}
+
+const saveMessages=(name,emailid,subject,message)=>
+{
+  var newContactForm= contactFormDB.push();  
+  newContactForm.set({
+    Name: name,
+    email: emailid,
+    Subject: subject,
+    Message: message,
+
+  });
+};
+
+const getElementVal= (id) =>{
+    return document.getElementById(id).value;
+};
