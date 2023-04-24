7b40d183f1ab0e914a4dd32e3ddf3a34cc31c3b7
SUMAN TANDAN <sumantandan1999@gmail.com>
2023-04-24 12:49:04 +0530

    edited

--- assets/js/like-button.js
+++ assets/js/like-button.js
@@ -1,29 +1,44 @@
 
 
 
+// // Get all the like and love buttons on the page
+// const buttons = document.querySelectorAll('.like-button, .love-button');
+
+// // Loop through each button and add a click event listener
+// buttons.forEach(button => {
+//     // Add a click event listener to the button
+//     button.addEventListener('click', () => {
+//         // Get the current number of likes or loves
+//         const count = button.querySelector('.like-count, .love-count');
+//         let currentCount = Number(count.textContent);
+//         // Check if the button has been liked or loved before
+//         const isLikedOrLoved = button.classList.contains('liked');
+//         // If the button is already liked or loved, remove the "liked" class and decrement the count
+//         if (isLikedOrLoved) {
+//             button.classList.remove('liked');
+//             // currentCount--;
+//         }
+//         // If the button hasn't been liked or loved, add the "liked" class and increment the count
+//         else {
+//             button.classList.add('liked');
+//             // currentCount++;
+//         }
+//         // Update the count on the button
+//         count.textContent = currentCount;
+//     });
+// });
+
+
 // Get all the like and love buttons on the page
 const buttons = document.querySelectorAll('.like-button, .love-button');
 
 // Loop through each button and add a click event listener
 buttons.forEach(button => {
-    // Add a click event listener to the button
-    button.addEventListener('click', () => {
-        // Get the current number of likes or loves
-        const count = button.querySelector('.like-count, .love-count');
-        let currentCount = Number(count.textContent);
-        // Check if the button has been liked or loved before
-        const isLikedOrLoved = button.classList.contains('liked');
-        // If the button is already liked or loved, remove the "liked" class and decrement the count
-        if (isLikedOrLoved) {
-            button.classList.remove('liked');
-            currentCount--;
-        }
-        // If the button hasn't been liked or loved, add the "liked" class and increment the count
-        else {
-            button.classList.add('liked');
-            currentCount++;
-        }
-        // Update the count on the button
-        count.textContent = currentCount;
-    });
+  // Add a click event listener to the button
+  button.addEventListener('click', () => {
+    // Check if the button has been liked or loved before
+    const isLikedOrLoved = button.classList.contains('liked');
+    // Toggle the "liked" class to change the color of the button
+    button.classList.toggle('liked', !isLikedOrLoved);
+  });
 });
